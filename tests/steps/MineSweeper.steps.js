87bc410c0c0927a2ba5036ef41a2efaefead4172
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-10-03 20:03:32 +0200

    Minor changes
    Signed-off-by: arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>

--- tests/steps/MineSweeper.steps.js
+++ tests/steps/MineSweeper.steps.js
@@ -81,7 +81,7 @@ Given(/^the cell at: \((\d+), (\d+)\) is questioned$/, async (rowNumber, columnN
     await cell.dblclick({ button: 'right' })
 })
 Given(/^a random board of: (.*) with (\d+) mines$/, async (size, mines) => {
-    await page.goto(`${url}?size=${size}&mines=${mines}`, { waitUntil: 'load' })
+    await page.goto(`${url}?random={size=${size},mines=${mines}}`, { waitUntil: 'load' })
 })
 /**
  * When
